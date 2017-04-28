e28d386b417c09bfb156074c28ff54ffb0482889
adryssonlima <adrysson.lima@gmail.com>
2017-04-28 17:45:36 -0300

    function javascript para remoção de semestre

--- views/curso/_form.php
+++ views/curso/_form.php
@@ -42,57 +42,59 @@ use yii\widgets\ActiveForm;
 
     function addSemestre(value) {
         var semestre = '<div class="semestre" id="semestre'+value+'">'+
-            '<div class="row">'+
-                '<div class="col-md-12">'+
-                    '<label>'+value+'ª semestre:</label>'+
+            '<fieldset>'+
+                '<legend>'+value+'ª semestre:&nbsp;&nbsp<span value="'+value+'" class="clicavel rm-semestre" title="Remover Semestre"><i class="fa fa-minus-circle text-danger" aria-hidden="true"></i></span></legend>'+
+                '<div class="row">'+
+                    '<div class="col-md-7"><label>Nome Disciplina:</label></div>'+
+                    '<div class="col-md-1"><label>CH/T:</label></div>'+
+                    '<div class="col-md-1"><label>CH/P:</label></div>'+
+                    '<div class="col-md-1"><label>CH/C:</label></div>'+
                 '</div>'+
-            '</div>'+
-            '<div class="row">'+
-                '<div class="col-md-7"><label>Nome Disciplina:</label></div>'+
-                '<div class="col-md-1"><label>CH/T:</label></div>'+
-                '<div class="col-md-1"><label>CH/P:</label></div>'+
-                '<div class="col-md-1"><label>CH/C:</label></div>'+
-            '</div>'+
-            '<div class="row">'+
-                '<div class="col-md-7">'+
-                    '<input type="text" class="form-control" name="disciplina" maxlength="100" aria-required="true">'+
+                '<div class="row">'+
+                    '<div class="col-md-7">'+
+                        '<input type="text" class="form-control" name="disciplina" maxlength="100" aria-required="true">'+
+                    '</div>'+
+                    '<div class="col-md-1">'+
+                        '<select class="form-control padding" name="cht">'+
+                            '<option value="0">0</option>'+
+                            '<option value="44">44</option>'+
+                            '<option value="88">88</option>'+
+                        '</select>'+
+                    '</div>'+
+                    '<div class="col-md-1">'+
+                        '<select class="form-control padding" name="chp">'+
+                            '<option value="0">0</option>'+
+                            '<option value="44">44</option>'+
+                            '<option value="88">88</option>'+
+                        '</select>'+
+                    '</div>'+
+                    '<div class="col-md-1">'+
+                        '<select class="form-control padding" name="chc">'+
+                            '<option value="0">0</option>'+
+                            '<option value="44">44</option>'+
+                            '<option value="88">88</option>'+
+                        '</select>'+
+                    '</div>'+
+                    '<div class="col-md-2">'+
+                        '<span class="clicavel" title="Adicionar Disciplina"><i class="fa fa-plus-circle fa-2x text-primary" aria-hidden="true"></i></span>'+
+                        '&nbsp;&nbsp;&nbsp<span class="clicavel" title="Remover Disciplina"><i class="fa fa-minus-circle fa-2x text-danger" aria-hidden="true"></i></span>'+
+                    '</div>'+
                 '</div>'+
-                '<div class="col-md-1">'+
-                    '<select class="form-control padding" name="cht">'+
-                        '<option value="0">0</option>'+
-                        '<option value="44">44</option>'+
-                        '<option value="88">88</option>'+
-                    '</select>'+
-                '</div>'+
-                '<div class="col-md-1">'+
-                    '<select class="form-control padding" name="chp">'+
-                        '<option value="0">0</option>'+
-                        '<option value="44">44</option>'+
-                        '<option value="88">88</option>'+
-                    '</select>'+
-                '</div>'+
-                '<div class="col-md-1">'+
-                    '<select class="form-control padding" name="chc">'+
-                        '<option value="0">0</option>'+
-                        '<option value="44">44</option>'+
-                        '<option value="88">88</option>'+
-                    '</select>'+
-                '</div>'+
-                '<div class="col-md-2">'+
-                    '<span class="clicavel" title="Adicionar Disciplina"><i class="fa fa-plus-circle fa-2x text-primary" aria-hidden="true"></i></span>'+
-                    '&nbsp;&nbsp;&nbsp<span class="clicavel" title="Remover Disciplina"><i class="fa fa-minus-circle fa-2x text-danger" aria-hidden="true"></i></span>'+
-                '</div>'+
-            '</div>'+
-        '</div>';
+            '</fieldset>'+
+        '</div> <br><br>';
         $('.semestres').append(semestre);
     }
-
 
     $('.add-semestre').click(function(){
         var value = $(this).attr('value');
         addSemestre(value);
         value++;
         $(this).attr('value', value);
+    });
+
+    $( ".semestres" ).on( "click", ".rm-semestre", function() {
+        var value = $(this).attr('value');
+        $('#semestre'+value).remove();
     });
 
 
