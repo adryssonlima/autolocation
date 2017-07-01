2126be56e91473b8ed39a2642db56532ed803a68
adryssonlima <adrysson.lima@gmail.com>
2017-07-01 20:44:54 -0300

    Tratamento para edição de semestre de uma turma

--- views/turma/update.php
+++ views/turma/update.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\bootstrap\Modal;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Turma */
@@ -19,14 +20,55 @@ $this->title = 'Editar Turma: ' . $model->identificador;
 
 </div>
 
+<?php
+Modal::begin([
+    "header" => "<h3 class='modal-titulo'>Atenção!</h3>",
+    "id" => "modal-aviso",
+    "size" => "modal-sm",
+    'clientOptions' => ['backdrop' => 'static', 'keyboard' => false],
+    'closeButton' => false
+]);
+echo "<div class='modal-conteudo center aviso' style='margin-top: 0px;'>";
+echo "<p><i class='fa fa-exclamation-triangle fa-2x' aria-hidden='true'></i><br>";
+echo "Ao alterar o semestre, todo o horário atual da turma será perdido.</p>";
+echo "<p>Tem certeza que deseja fazer isso?</p>";
+echo "</div><br>";
+echo "<button type='button' class='btn btn-default cancel-alter-semestre' data-dismiss='modal'>Cancelar</button>";
+echo "&nbsp;<button type='button' class='btn btn-danger pull-right confirm-alter-semestre' data-dismiss='modal'><i class='fa fa-check' aria-hidden='true'></i> Tenho certeza</button>";
+Modal::end();
+?>
+
+<style>
+    .padding {
+        padding: 1px;
+    }
+	.margin-bottom {
+		margin-bottom: 7px;
+	}
+    .center {
+        text-align: center;
+        margin-top: 30px;
+    }
+    .aviso {
+        color: #999999;
+        font-weight: bold;
+    }
+</style>
+
 <script>
     
     $(document).ready(function(){
+
+        $("#turma-curso").prop("disabled", true);
+        $("#turma-turno").prop("disabled", true);
 
         getSemestres("<?= $model->curso ?>", "<?= $model->semestre ?>");
 
         $("#conf-dados-turma").prop("disabled",false);
         $("#valida").text("");
+
+        var turno = $('#turma-turno').val();
+        createTabelaHorario(turno);
 
     });
 
@@ -38,10 +80,15 @@ $this->title = 'Editar Turma: ' . $model->identificador;
         getHorariosOcupados();
     });
 
-    $('#conf-dados-turma').click(function() {
-        var turno = $('#turma-turno').val();
-        createTabelaHorario(turno);
-        getHorariosOcupados();
+    $("#turma-semestre").change(function() { //verifica a alteração de semestre
+        $("#modal-aviso").modal("show");
+        $(".cancel-alter-semestre").click(function(){
+            $("#turma-semestre").val("<?= $model->semestre ?>");
+        });
+        $(".confirm-alter-semestre").click(function(){
+            $("#tbody-periodos").find(".info-turma").remove();
+            $("#tbody-periodos").find(".info-turma-disciplina-sala").text("");
+        });
     });
 
     $("#table-horario").on( "click", "a", function() {
@@ -136,25 +183,13 @@ $this->title = 'Editar Turma: ' . $model->identificador;
                 var periodos = dados['periodos'];
                                 
                 if (montaTabelaHorario(dias_da_semana, periodos)) {
-                    var horario = $.parseJSON('<?= $horario ?>');
-                    preencheTabelaHorário(horario);
+                    getHorariosOcupados();
                 }
             },
             error: function () {
                 console.log("Erro ao submeter requisição Ajax");
             }
         });
-    }
-
-    function preencheTabelaHorário(horario) { //Pega os dados de horários da turma e preenche a tabela
-        horario.forEach(function(val) {
-            //console.log(val);
-            $("#td"+val.semana+val.periodo).find("#span"+val.semana+val.periodo).html(val.nome_disciplina+"<br>"+val.identificador_sala);
-            $("#td"+val.semana+val.periodo).css('text-align','center');                        
-            $("#td"+val.semana+val.periodo).append("<hidden id='hidden"+val.semana+val.periodo+"' dia='"+val.semana+"' periodo='"+val.periodo+"' sala='"+val.sala+"' disciplina='"+val.disciplina+"'></hidden>");
-            $("#td"+val.semana+val.periodo).append("<a id='linkremove"+val.semana+val.periodo+"' id_dia='"+val.semana+"' id_periodo='"+val.periodo+"' href='#' class='pull-right text-danger' style='margin-right: 2px;' title='Remover'> <span class='glyphicon glyphicon-trash'></span> </a>");
-        });
-        return true;
     }
 
     function montaTabelaHorario(dias_da_semana, periodos) {
@@ -167,7 +202,7 @@ $this->title = 'Editar Turma: ' . $model->identificador;
         $.each(periodos, function(keyPeriodo, periodo) {
             $('#tbody-periodos').append("<tr id='" + periodo['id'] + "'><th class='th-center'>" + periodo['identificador'] + "<br>" + periodo['intervalo'] + "</th></tr>");
             $.each(dias_da_semana, function(keyDia, dia) {
-                $('#'+periodo['id']).append("<td id='td" + dia['id']+periodo['id'] + "' class='tdhover'> <span id='span" + dia['id']+periodo['id'] + "'></span> <a id='link" + dia['id']+periodo['id'] + "' id_dia='" + dia['id'] + "' id_periodo='" + periodo['id'] + "' href='#' class='pull-right text-success' data-toggle='modal' data-target='#myModal' title='Editar'> <span class='glyphicon glyphicon-pencil'></span> </a> </td>");
+                $('#'+periodo['id']).append("<td id='td" + dia['id']+periodo['id'] + "' class='tdhover'> <span class='info-turma-disciplina-sala' id='span" + dia['id']+periodo['id'] + "'></span> <a id='link" + dia['id']+periodo['id'] + "' id_dia='" + dia['id'] + "' id_periodo='" + periodo['id'] + "' href='#' class='pull-right text-success' data-toggle='modal' data-target='#myModal' title='Editar'> <span class='glyphicon glyphicon-pencil'></span> </a> </td>");
             });
         });
         return true;
@@ -183,19 +218,49 @@ $this->title = 'Editar Turma: ' . $model->identificador;
                 id: null
             },
             success: function (data) {
-                var dados = $.parseJSON(data);
-                console.log(dados);
-                $("#table-horario").find("span").text("");
-                $.each(dados, function(index, value) {
-                    $("#span"+value).text("Sem Salas Disponíveis").css('color', 'red');
-                    $("#td"+value).css('text-align','center');
-                    $("#link"+value).remove();
-                });
+                var ocupados = $.parseJSON(data);
+                //console.log(ocupados);
+                var horario = $.parseJSON('<?= $horario ?>');
+                //console.log(horario);
+                if (setTableHorariosCocupados(ocupados, horario)) {
+                    preencheTabelaHorario(horario);
+                }
             },
             error: function () {
                 console.log("Erro ao submeter requisição Ajax");
             }
         });
+    }
+
+    function preencheTabelaHorario(horario) { //Pega os dados de horários da turma e preenche a tabela
+        horario.forEach(function(val) {
+            //console.log(val);
+            $("#td"+val.semana+val.periodo).find("#span"+val.semana+val.periodo).html(val.nome_disciplina+"<br>"+val.identificador_sala);
+            $("#td"+val.semana+val.periodo).css('text-align','center');                        
+            $("#td"+val.semana+val.periodo).append("<hidden class='info-turma' id='hidden"+val.semana+val.periodo+"' dia='"+val.semana+"' periodo='"+val.periodo+"' sala='"+val.sala+"' disciplina='"+val.disciplina+"'></hidden>");
+            $("#td"+val.semana+val.periodo).append("<a id='linkremove"+val.semana+val.periodo+"' id_dia='"+val.semana+"' id_periodo='"+val.periodo+"' href='#' class='pull-right text-danger info-turma' style='margin-right: 2px;' title='Remover'> <span class='glyphicon glyphicon-trash'></span> </a>");
+        });
+        return true;
+    }
+
+    function setTableHorariosCocupados(ocupados, horario) {
+
+        horario.forEach(function(val) { //remove dos horarios ocupados os horarios da turma a ser editada
+            let = semana_periodo = val.semana+val.periodo;
+            $.each(ocupados, function(i, v) {
+                if (semana_periodo == v)
+                    delete ocupados[i];
+            });
+        });
+
+        $("#table-horario").find("span").text("");
+        $.each(ocupados, function(index, value) {
+            $("#span"+value).removeClass("info-turma-disciplina-sala");
+            $("#span"+value).text("Sem Salas Disponíveis").css('color', 'red');
+            $("#td"+value).css('text-align','center');
+            $("#link"+value).remove();
+        });
+        return true;
     }
 
     function getSalasDisciplinas(dia, periodo, curso, semestre) {
@@ -220,7 +285,16 @@ $this->title = 'Editar Turma: ' . $model->identificador;
                 $.each(disciplinas, function(index, value) {
                     $("#modal-disciplina").append($("<option></option>").attr("value", index).text(value));
                 });
-                console.log(dados);
+
+                var horario = $.parseJSON('<?= $horario ?>');
+                horario.forEach(function(val){
+                    if (val.semana == dia && val.periodo == periodo) {
+                        $("#modal-sala").append($("<option></option>").attr("value", val.sala).text(val.identificador_sala));
+                        $("#modal-sala").val(val.sala);
+                        $("#modal-disciplina").val(val.disciplina);
+                    }
+                });
+                //console.log(dados);
             },
             error: function () {
                 console.log("Erro ao submeter requisição Ajax");
